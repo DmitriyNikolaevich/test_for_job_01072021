1fc813bb2ff8b71c43809cc3b309657b60e6b1f5
DmitriyNikolaevich <dnkaplun1987@gmail.com>
2021-07-04 23:12:15 +0300

    deploy 8

--- src/API.js
+++ src/API.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const instance = axios.create({
     withCredentials: true,
-    baseURL: 'https://test-for-job-01072021.herokuapp.com/'
+    baseURL: 'https://test-for-job-01072021-node.herokuapp.com/'
 })
